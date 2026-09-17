030be5b1aad58cd7d97f1cf7d5cfc3986d871e4f
secoma13 <sergiomartinnieto13@gmail.com>
2026-09-17 20:23:46 +0200

    First animation achieved

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.animation import PillowWriter
 import math
 from scipy.integrate import solve_ivp
 
@@ -51,7 +51,7 @@ def get_mass_lengths():
     m1 : float = input("Mass of the first pendulum in kgs: ")
     m2 : float = input("Mass of the second pendulum in kgs: ")
     l1 : float = input("Length of the rope between the ceiling and the first pendulum in meters: ")
-    l2 : float = input("Length of the rope between the pendulum in meters")
+    l2 : float = input("Length of the rope between the pendulum in meters: ")
 
     return (m1, m2, l1, l2)
 
@@ -76,7 +76,7 @@ def ode_fun(t, y, m1, m2, l1, l2):
     # ODE
     denom = l1*(2*m1 + m2*(1 - math.cos(2*(y[0]-y[1]))))
 
-    dot_y2 = 1/denom*(-g(*(2*m1 + m2)*math.sin(y[0]) - m2*math.sin(y[0]-2*y[1])) - 2*m2*math.sin(y[0] - y[1])*((y[3]**2)*l2 + (y[2]**2)*l1*math.cos(y[0] - y[1])))
+    dot_y2 = 1/denom*(-g*((2*m1 + m2)*math.sin(y[0]) - m2*math.sin(y[0]-2*y[1])) - 2*m2*math.sin(y[0] - y[1])*((y[3]**2)*l2 + (y[2]**2)*l1*math.cos(y[0] - y[1])))
     dot_y3 = 1/denom*(2*math.sin(y[0] - y[1])*((y[2]**2)*l1*(m1 + m2) + g*(m1 + m2)*math.cos(y[0]) + (y[3]**2)*l2*m2*math.cos(y[0] - y[1])))
 
     return [y[2], y[3], dot_y2, dot_y3]
@@ -85,7 +85,7 @@ def radians(theta :  float) -> float:
     """
     Converts an angle in degrees to radians 
     """
-    return theta*2*math.py/180
+    return theta*2*math.pi/180
 
 def coordinate_transf(l1 : float, l2 : float, theta1 : float, theta2 : float):
     """
@@ -110,14 +110,19 @@ def coordinate_transf(l1 : float, l2 : float, theta1 : float, theta2 : float):
     return (x1, y1, x1 + l2*math.cos(theta2), y1 - l2*math.sin(theta2))
 
 
-
 def main():
 
     # Input of the system
-    m1, m2, l1, l2 = get_mass_lengths()
+    # m1, m2, l1, l2 = get_mass_lengths()
+
+    # BORRAR DESPUÉS
+    m1 = 2; m2 = 3; l1 = 2; l2 = 1
 
     # Initial conditions
-    theta1, theta2, theta1_dot, theta2_dot = get_init_cond()
+    # theta1, theta2, theta1_dot, theta2_dot = get_init_cond()
+
+    # BORRAR DESPUÉS
+    theta1 = 60; theta2 = -30; theta1_dot = 20; theta2_dot = 0 
 
     # Create the pendulums
     p1 = Pendulum(m1, l1, radians(theta1), radians(theta1_dot))
@@ -127,15 +132,32 @@ def main():
     # sol is an object. sol.t is the time points at which we get the solutions and sol.y are the solutions (it is an np.array)
     # y0 is the angle of the first pendulum, y1 of the second, and y2 and y3 are theire respective velocites.
     # For the plot only the positions matter
-    sol = solve_ivp(fun = ode_fun, t_span = (0, 30), t_eval = np.linspace(0, 30, 1000), y0 = [theta1, theta2, theta1_dot, theta2_dot], args = (m1, m2, l1, l2))
+    sol = solve_ivp(fun = ode_fun, t_span = (0, 30), t_eval = np.linspace(0, 30, 3000), y0 = [theta1, theta2, theta1_dot, theta2_dot], args = (m1, m2, l1, l2))
 
     # Plot the solution
     # We fix a coordinate system where (0,0) is the point where the rope l1 touches the ceiling
+    # We create the figures. We plot both the animation displaying both pendulums and the evolution of the kinetic and potential energies over time
+    fig = plt.figure()
+    l, = plt.plot([], [], color = "black", linestyle = "-")
+    ll,  = plt.plot([], [], "blue", marker = 'o')
 
-    for i in range(0:1000):
-        x1_t, y1_t, x2_t, y2_t = coordinate_transf(l1, l2, sol.y[0, i], sol.y[1, i])
-        pass
+    # We set the axis (we add the 1.5 to see both pendulums at every point)
+    l12 = l1 + l2 + 1.5
+    plt.xlim(-l12, l12)
+    plt.ylim(-l12, l12)
 
+    # We create the animation
+    metadata = dict(title = "Double Pendulum", artist = "Sergio Martín Nieto")
+    writer = PillowWriter(fps = 20, metadata = metadata)
+
+    # Explicar que hace esta sintaxis
+    with writer.saving(fig, "DoublePendulum.gif", 75):
+        for i in range(700):
+            x1_t, y1_t, x2_t, y2_t = coordinate_transf(l1, l2, sol.y[0, i], sol.y[1, i])
+            l.set_data([0, x1_t, x2_t], [0, y1_t, y2_t])
+            ll.set_data([x1_t, x2_t], [y1_t, y2_t])
+
+            writer.grab_frame()
 
 
 if __name__ == "__main__":
